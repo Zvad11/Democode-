36b3bae2f9fe68468fce016ad676eaa82c28118b
Zviko Mutero <117074386+Zvad11@users.noreply.github.com>
2023-01-11 15:55:51 +0200

    My date function
    Basic method that displays the current date and time.

---
+++ myDate.js
@@ -0,0 +1,17 @@
+//function  dol (abc){
+  //  var value="Hello Sedrick we are done"
+    //abc(value)
+
+//}
+
+//function abc(value){
+//console.log(value)
+//}
+//dol(abc)
+
+ myDate = ()=>{
+    var day= new Date;
+    console.log(day);
+ }
+
+ myDate();
